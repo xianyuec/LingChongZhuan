de1570868dd1a07ef957cc52df4ae3d3c0bde120
xianyue <xianyuec@163.com>
2017-03-27 13:05:06 +0800

    修复0.1版本 血条显示bug

--- assets/Script/Hero.js
+++ assets/Script/Hero.js
@@ -11,7 +11,7 @@ cc.Class({
     },
     
     updateShowBloodRate: function (rate) {
-        this.bloodBar.progress = rate;
+        this.bloodBar.node.getChildByName("bar").width = Math.floor(this.node.width * rate);
     },
 
     dead: function() {
